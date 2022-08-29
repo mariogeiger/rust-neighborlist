73b3c6618eaa0c0202b93bf84f5b4af2586fd7a9
Mario Geiger <geiger.mario@gmail.com>
2022-08-28 22:50:54 -0400

    first commit

---
+++ src/lib.rs
@@ -0,0 +1,102 @@
+use itertools::Itertools;
+use numpy::ndarray::{Array1, Array2, ArrayView2};
+use numpy::{IntoPyArray, PyArray1, PyArray2, PyReadonlyArray2};
+use pyo3::{pymodule, types::PyModule, PyResult, Python};
+use std::collections::HashMap;
+
+#[pymodule]
+fn rust_neighborlist(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
+    fn neighbor_list(
+        positions: ArrayView2<'_, f64>,
+        cutoff: f64,
+        self_interaction: bool,
+    ) -> (Array1<i32>, Array1<i32>, Array1<f64>, Array2<f64>) {
+        let n = positions.shape()[0];
+        let box_size = 1.01 * cutoff;
+        let mut boxes = HashMap::new();
+
+        let get_key = |x: f64, y: f64, z: f64| {
+            (
+                (x / box_size) as i32,
+                (y / box_size) as i32,
+                (z / box_size) as i32,
+            )
+        };
+
+        for i in 0..n {
+            let key = get_key(positions[(i, 0)], positions[(i, 1)], positions[(i, 2)]);
+            if !boxes.contains_key(&key) {
+                boxes.insert(key, Vec::new());
+            }
+            boxes.get_mut(&key).unwrap().push(i);
+        }
+
+        let mut src: Vec<i32> = Vec::new();
+        let mut dst: Vec<i32> = Vec::new();
+        let mut dist: Vec<f64> = Vec::new();
+        let mut rel: Vec<[f64; 3]> = Vec::new();
+
+        for (&key, value) in boxes.iter() {
+            for &i in value {
+                let ix = positions[(i, 0)];
+                let iy = positions[(i, 1)];
+                let iz = positions[(i, 2)];
+                let it = (-1..2).cartesian_product(-1..2).cartesian_product(-1..2);
+                for ((sx, sy), sz) in it {
+                    let skey = (key.0 + sx, key.1 + sy, key.2 + sz);
+                    if let Some(svalue) = boxes.get(&skey) {
+                        for &j in svalue {
+                            let jx = positions[(j, 0)];
+                            let jy = positions[(j, 1)];
+                            let jz = positions[(j, 2)];
+                            let dx = jx - ix;
+                            let dy = jy - iy;
+                            let dz = jz - iz;
+                            let r2 = dx * dx + dy * dy + dz * dz;
+                            if r2 < cutoff * cutoff {
+                                if self_interaction || i != j {
+                                    src.push(i as i32);
+                                    dst.push(j as i32);
+                                    dist.push(r2.sqrt());
+                                    rel.push([dx, dy, dz]);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        (
+            Array1::from(src),
+            Array1::from(dst),
+            Array1::from(dist),
+            Array2::from(rel),
+        )
+    }
+
+    #[pyfn(m)]
+    #[pyo3(name = "neighbor_list")]
+    fn neighbor_list_py<'py>(
+        py: Python<'py>,
+        positions: PyReadonlyArray2<'_, f64>,
+        cutoff: f64,
+        self_interaction: bool,
+    ) -> (
+        &'py PyArray1<i32>,
+        &'py PyArray1<i32>,
+        &'py PyArray1<f64>,
+        &'py PyArray2<f64>,
+    ) {
+        let positions = positions.as_array();
+        let (src, dst, dist, rel) = neighbor_list(positions.view(), cutoff, self_interaction);
+        (
+            src.into_pyarray(py),
+            dst.into_pyarray(py),
+            dist.into_pyarray(py),
+            rel.into_pyarray(py),
+        )
+    }
+
+    Ok(())
+}
---
+++ tests/test_neighborlist.py
@@ -0,0 +1,31 @@
+from rust_neighborlist import neighbor_list
+import numpy as np
+import ase
+import ase.neighborlist
+
+
+def test_neighbor_list():
+    pos = np.random.uniform(-4.0, 3.0, (100, 3))
+    cutoff = 2.0
+    i1, j1, d1, D1 = neighbor_list(pos, cutoff, False)
+
+    i = np.lexsort((j1, i1))
+    i1 = i1[i]
+    j1 = j1[i]
+    d1 = d1[i]
+    D1 = D1[i]
+
+    a = ase.Atoms(positions=pos)
+    i2, j2, d2, D2 = ase.neighborlist.neighbor_list("ijdD", a, cutoff, False)
+
+    i = np.lexsort((j2, i2))
+    i2 = i2[i]
+    j2 = j2[i]
+    d2 = d2[i]
+    D2 = D2[i]
+
+    assert len(i1) == len(i2)
+    np.testing.assert_array_equal(i1, i2)
+    np.testing.assert_array_equal(j1, j2)
+    np.testing.assert_allclose(d1, d2)
+    np.testing.assert_allclose(D1, D2)
