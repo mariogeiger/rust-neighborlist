ed04d7c781217f5c52fad4196f631e9328cbd44e
Mario Geiger <geiger.mario@gmail.com>
2022-08-28 22:59:22 -0400

    simplify code

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,3 @@
-use itertools::Itertools;
 use numpy::ndarray::{Array1, Array2, ArrayView2};
 use numpy::{IntoPyArray, PyArray1, PyArray2, PyReadonlyArray2};
 use pyo3::{pymodule, types::PyModule, PyResult, Python};
@@ -15,16 +14,16 @@ fn rust_neighborlist(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
         let box_size = 1.01 * cutoff;
         let mut boxes = HashMap::new();
 
-        let get_key = |x: f64, y: f64, z: f64| {
-            (
+        for i in 0..n {
+            let x = positions[(i, 0)];
+            let y = positions[(i, 1)];
+            let z = positions[(i, 2)];
+
+            let key = (
                 (x / box_size) as i32,
                 (y / box_size) as i32,
                 (z / box_size) as i32,
-            )
-        };
-
-        for i in 0..n {
-            let key = get_key(positions[(i, 0)], positions[(i, 1)], positions[(i, 2)]);
+            );
             if !boxes.contains_key(&key) {
                 boxes.insert(key, Vec::new());
             }
@@ -41,24 +40,27 @@ fn rust_neighborlist(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
                 let ix = positions[(i, 0)];
                 let iy = positions[(i, 1)];
                 let iz = positions[(i, 2)];
-                let it = (-1..2).cartesian_product(-1..2).cartesian_product(-1..2);
-                for ((sx, sy), sz) in it {
-                    let skey = (key.0 + sx, key.1 + sy, key.2 + sz);
-                    if let Some(svalue) = boxes.get(&skey) {
-                        for &j in svalue {
-                            let jx = positions[(j, 0)];
-                            let jy = positions[(j, 1)];
-                            let jz = positions[(j, 2)];
-                            let dx = jx - ix;
-                            let dy = jy - iy;
-                            let dz = jz - iz;
-                            let r2 = dx * dx + dy * dy + dz * dz;
-                            if r2 < cutoff * cutoff {
-                                if self_interaction || i != j {
-                                    src.push(i as i32);
-                                    dst.push(j as i32);
-                                    dist.push(r2.sqrt());
-                                    rel.push([dx, dy, dz]);
+                for sx in -1..2 {
+                    for sy in -1..2 {
+                        for sz in -1..2 {
+                            let skey = (key.0 + sx, key.1 + sy, key.2 + sz);
+                            if let Some(svalue) = boxes.get(&skey) {
+                                for &j in svalue {
+                                    let jx = positions[(j, 0)];
+                                    let jy = positions[(j, 1)];
+                                    let jz = positions[(j, 2)];
+                                    let dx = jx - ix;
+                                    let dy = jy - iy;
+                                    let dz = jz - iz;
+                                    let r2 = dx * dx + dy * dy + dz * dz;
+                                    if r2 < cutoff * cutoff {
+                                        if self_interaction || i != j {
+                                            src.push(i as i32);
+                                            dst.push(j as i32);
+                                            dist.push(r2.sqrt());
+                                            rel.push([dx, dy, dz]);
+                                        }
+                                    }
                                 }
                             }
                         }
