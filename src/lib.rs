ea9db05968e57b7426215bf24c2c9533b5c7fb23
Mario Geiger <geiger.mario@gmail.com>
2022-08-29 11:05:01 -0400

    move function out of module

--- src/lib.rs
+++ src/lib.rs
@@ -3,60 +3,57 @@ use numpy::{IntoPyArray, PyArray1, PyArray2, PyReadonlyArray2};
 use pyo3::{pymodule, types::PyModule, PyResult, Python};
 use std::collections::HashMap;
 
-#[pymodule]
-fn neighborlist(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
-    fn neighbor_list(
-        positions: ArrayView2<'_, f64>,
-        cutoff: f64,
-        self_interaction: bool,
-    ) -> (Array1<i32>, Array1<i32>, Array1<f64>, Array2<f64>) {
-        let n = positions.shape()[0];
-        let box_size = 1.001 * cutoff;
-        let mut boxes = HashMap::new();
+fn neighbor_list(
+    positions: ArrayView2<'_, f64>,
+    cutoff: f64,
+    self_interaction: bool,
+) -> (Array1<i32>, Array1<i32>, Array1<f64>, Array2<f64>) {
+    let n = positions.shape()[0];
+    let box_size = 1.001 * cutoff;
+    let mut boxes = HashMap::new();
 
-        for i in 0..n {
-            let x = positions[(i, 0)];
-            let y = positions[(i, 1)];
-            let z = positions[(i, 2)];
+    for i in 0..n {
+        let x = positions[(i, 0)];
+        let y = positions[(i, 1)];
+        let z = positions[(i, 2)];
 
-            let key = (
-                (x / box_size) as i32,
-                (y / box_size) as i32,
-                (z / box_size) as i32,
-            );
-            boxes.entry(key).or_insert(Vec::new()).push(i);
-        }
+        let key = (
+            (x / box_size) as i32,
+            (y / box_size) as i32,
+            (z / box_size) as i32,
+        );
+        boxes.entry(key).or_insert(Vec::new()).push(i);
+    }
 
-        let mut src: Vec<i32> = Vec::new();
-        let mut dst: Vec<i32> = Vec::new();
-        let mut dist: Vec<f64> = Vec::new();
-        let mut rel: Vec<[f64; 3]> = Vec::new();
+    let mut src: Vec<i32> = Vec::new();
+    let mut dst: Vec<i32> = Vec::new();
+    let mut dist: Vec<f64> = Vec::new();
+    let mut rel: Vec<[f64; 3]> = Vec::new();
 
-        for (&key, value) in boxes.iter() {
-            for &i in value {
-                let ix = positions[(i, 0)];
-                let iy = positions[(i, 1)];
-                let iz = positions[(i, 2)];
-                for sx in -1..2 {
-                    for sy in -1..2 {
-                        for sz in -1..2 {
-                            let skey = (key.0 + sx, key.1 + sy, key.2 + sz);
-                            if let Some(svalue) = boxes.get(&skey) {
-                                for &j in svalue {
-                                    let jx = positions[(j, 0)];
-                                    let jy = positions[(j, 1)];
-                                    let jz = positions[(j, 2)];
-                                    let dx = jx - ix;
-                                    let dy = jy - iy;
-                                    let dz = jz - iz;
-                                    let r2 = dx * dx + dy * dy + dz * dz;
-                                    if r2 < cutoff * cutoff {
-                                        if self_interaction || i != j {
-                                            src.push(i as i32);
-                                            dst.push(j as i32);
-                                            dist.push(r2.sqrt());
-                                            rel.push([dx, dy, dz]);
-                                        }
+    for (&key, value) in boxes.iter() {
+        for &i in value {
+            let ix = positions[(i, 0)];
+            let iy = positions[(i, 1)];
+            let iz = positions[(i, 2)];
+            for sx in -1..2 {
+                for sy in -1..2 {
+                    for sz in -1..2 {
+                        let skey = (key.0 + sx, key.1 + sy, key.2 + sz);
+                        if let Some(svalue) = boxes.get(&skey) {
+                            for &j in svalue {
+                                let jx = positions[(j, 0)];
+                                let jy = positions[(j, 1)];
+                                let jz = positions[(j, 2)];
+                                let dx = jx - ix;
+                                let dy = jy - iy;
+                                let dz = jz - iz;
+                                let r2 = dx * dx + dy * dy + dz * dz;
+                                if r2 < cutoff * cutoff {
+                                    if self_interaction || i != j {
+                                        src.push(i as i32);
+                                        dst.push(j as i32);
+                                        dist.push(r2.sqrt());
+                                        rel.push([dx, dy, dz]);
                                     }
                                 }
                             }
@@ -65,15 +62,18 @@ fn neighborlist(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
                 }
             }
         }
-
-        (
-            Array1::from(src),
-            Array1::from(dst),
-            Array1::from(dist),
-            Array2::from(rel),
-        )
     }
 
+    (
+        Array1::from(src),
+        Array1::from(dst),
+        Array1::from(dist),
+        Array2::from(rel),
+    )
+}
+
+#[pymodule]
+fn neighborlist(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
     /// neighbor_list(positions, cutoff, /, self_interaction)
     /// --
     ///
