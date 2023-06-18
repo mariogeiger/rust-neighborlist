4cc153ea86c94a8be9bf51782b37f26655693bce
Mario Geiger <geiger.mario@gmail.com>
2023-06-18 15:24:12 -0400

    fix nox

--- noxfile.py
+++ noxfile.py
@@ -3,6 +3,6 @@ import nox
 
 @nox.session
 def tests(session):
-    session.install("pip", "numpy", "pytest", "ase")
+    session.install("pip", "numpy", "pytest", "matscipy")
     session.run("pip", "install", ".", "-v")
     session.run("pytest")
